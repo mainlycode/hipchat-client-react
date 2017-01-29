361798c6c125e80f62174a63476c6f1bba1fc97f
Sander van Thillo <othillo@othillo.nl>
2017-01-29 17:54:57 +0100

    removed bootstrap

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
-    $loader = require_once $file;
-    $loader->add('MainlyCode', __DIR__);
-} else {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
-
